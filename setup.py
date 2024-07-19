05e2c2dd687c2a514d8e32b750c8caab1cef0712
MikiAsefa <mertigenet@gmail.com>
2024-07-19 03:19:10 +0300

    minor bug fix

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 """Configure installation for pydemy package."""
 
-from pathlib import Path
-from typing import List
-
 from setuptools import find_packages, setup
 
 
 def get_long_description() -> str:
     """Reads the long description from README.md."""
-    with Path("README.md").open(encoding="utf-8") as f:
+    with open("README.md", encoding="utf-8") as f:
         return f.read()
-
-
-def get_requirements() -> List[str]:
-    """Reads requirements from requirements.txt and returns a list of dependencies."""
-    with Path("requirements.txt").open() as requirements:
-        return [line.strip() for line in requirements]
 
 
 setup(
@@ -28,5 +19,5 @@ setup(
     author_email="mertigenet@gmail.com",
     url="https://github.com/robelasefa/pydemy",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=get_requirements(),
+    install_requires=["requests~=2.32.3", "pydantic~=2.8.2"],
 )
